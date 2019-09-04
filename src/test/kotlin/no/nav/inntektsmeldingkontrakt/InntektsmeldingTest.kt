111efc5c66e3dd09072ab596697e3a8553e4631e
Aleksander Andresen <aleksander.andresen@nav.no>
2019-09-04 11:04:22 +0200

    Fikser kompileringsfeil i test

--- src/test/kotlin/no/nav/inntektsmeldingkontrakt/InntektsmeldingTest.kt
+++ src/test/kotlin/no/nav/inntektsmeldingkontrakt/InntektsmeldingTest.kt
@@ -2,13 +2,14 @@ package no.nav.inntektsmeldingkontrakt
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 import java.time.LocalDate
 class InntektsmeldingTest {
 
     @Test
     fun getVirksomhetsnummer() {
-        val r = Refusjon(15f, LocalDate.now())
-        assertEquals(15f, r.beloepPrMnd)
+        val r = Refusjon(BigDecimal.TEN, LocalDate.now())
+        assertEquals(BigDecimal.TEN, r.beloepPrMnd)
     }
 
 }
